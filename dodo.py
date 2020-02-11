85bfd416cca7ed3e25b50215a46e771ec806b151
Dominik Stańczak <dominik.stanczak@ifpilm.pl>
2020-02-11 08:17:29 +0100

    Doit script

---
+++ dodo.py
@@ -0,0 +1,51 @@
+import pathlib
+import click
+import subprocess
+home = pathlib.Path.home()
+
+def create_symlink(source, target):
+    source = pathlib.Path(source)
+    target = pathlib.Path(target)
+    diff = subprocess.run(["diff", target, source], capture_output='stdout').stdout.decode()
+    if diff:
+        click.echo(diff)
+        # if click.confirm(f'Do you want to replace {target} with a symlink to {source}?'):
+        if True:
+            click.echo('Well done!')
+            target.rename(target.with_suffix(".backup"))
+            target.symlink_to(source)
+    else:
+        click.echo(f"{source} and {target} have identical contents.")
+
+def create_symlink2(dependencies, targets):
+    for source, target in zip(dependencies, targets):
+        create_symlink(source, target)
+
+def task_create_symlinks():
+    for source, target in [
+            ("vim/.vimrc", home / ".vimrc"),
+            ("vim/.config/nvim/init.vim", home / "/.config/nvim/init.vim"),
+            ("scripts/workhours.py", home / "/Code/scripts/workhours.py"),
+            # ("echo "Link git/.gitconfig manually!"
+            ("git/.gitignore_global", home / "/.gitignore_global"),
+            ("conda/.condarc", home / "/.condarc"),
+            ("bash/.bash_aliases", home / "/.bash_aliases"),
+            ("bash/.bash_completion", home / "/.bash_completion"),
+            ("bash/.bash_profile", home / "/.bash_profile"),
+            ("bash/.bashrc", home / "/.bashrc"),
+            # ("echo "Link bash/.bashrc_local manually!"
+            ("ipython/profile_default/ipython_config.py", home / "/.ipython/profile_default/ipython_config.py"),
+            ("ipython/profile_default/ipython_kernel_config.py", home / "/.ipython/profile_default/ipython_kernel_config.py"),
+            # ("echo "Link ~/.ipython/profile_default/startup manually!"
+            ("zsh/.zshrc", home / ".zshrc"), 
+            # ("zsh/.zsh/", home / ".zsh/"), 
+             # link pre-commit/.pre-commit-config.yaml manually
+            ("pubs/.pubsrc", home / ".pubsrc"),
+    ]:
+        yield {
+            'name': source,
+            'file_dep': [pathlib.Path.cwd() / source],
+            'targets': [target],
+            'actions': [create_symlink2],
+        }
+
