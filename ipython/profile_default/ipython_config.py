2ab58dc05f2e8fd4245408459ee044ae281da6f5
Dominik Stańczak <dominik.stanczak@ifpilm.pl>
2020-02-19 09:58:44 +0100

    Ipython startup

--- ipython/profile_default/ipython_config.py
+++ ipython/profile_default/ipython_config.py
@@ -29,7 +29,7 @@
 #c.InteractiveShellApp.exec_files = []
 
 ## lines of code to run at IPython startup.
-c.InteractiveShellApp.exec_lines = ["import numpy as np", "import matplotlib.pyplot as plt", "import astropy.units as u"]
+#c.InteractiveShellApp.exec_lines = []
 
 ## A list of dotted module names of IPython extensions to load.
 #c.InteractiveShellApp.extensions = []
