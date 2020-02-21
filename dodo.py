465718711fd72c9891d9fa2e7f791285b0e1872b
Dominik Stańczak <stanczakdominik@gmail.com>
2020-02-21 17:45:13 +0100

    Separate config files per machine

--- dodo.py
+++ dodo.py
@@ -1,6 +1,7 @@
 import pathlib
 import click
 import subprocess
+import socket
 
 home = pathlib.Path.home()
 
@@ -33,16 +34,13 @@ def task_create_symlinks():
         ("vim/.vimrc", home / ".config/nvim/.vimrc"),
         (home / ".config/nvim/.vimrc", home / ".vimrc",),
         ("vim/.config/nvim/init.vim", home / ".config/nvim/init.vim"),
-        # ("echo "Link git/.gitconfig manually!"
         ("git/.gitignore_global", home / ".gitignore_global"),
         ("conda/.condarc", home / ".condarc"),
         ("bash/.bash_aliases", home / ".bash_aliases"),
         ("bash/.bash_completion", home / ".bash_completion"),
         ("bash/.bash_profile", home / ".bash_profile"),
         ("bash/.bashrc", home / ".bashrc"),
-        ("wtfutil/config.yml", home / ".config/wtf/config.yml"), # TODO split
         ("jrnl/.config/jrnl/jrnl.yaml", home / ".config/jrnl/jrnl.yaml"),
-        # ("echo "Link bash/.bashrc_local manually!"
         (
             "ipython/profile_default/ipython_config.py",
             home / ".ipython/profile_default/ipython_config.py",
@@ -51,11 +49,10 @@ def task_create_symlinks():
             "ipython/profile_default/ipython_kernel_config.py",
             home / ".ipython/profile_default/ipython_kernel_config.py",
         ),
-        # ("echo "Link ~/.ipython/profile_default/startup manually!"
         ("zsh/.zshrc", home / ".zshrc"),
         ("zsh/.zshenv", home / ".zshenv"),
-        # ("zsh/.zsh/", home / ".zsh/"),
-        # link pre-commit/.pre-commit-config.yaml manually
+        # ("zsh/.zsh/", home / ".zsh/"),   is a directory
+        # ("echo "Link ~/.ipython/profile_default/startup manually!" is a directory
         ("pubs/.pubsrc", home / ".pubsrc"),
     ]:
         yield {
@@ -64,3 +61,30 @@ def task_create_symlinks():
             "targets": [target],
             "actions": [create_symlink2],
         }
+
+
+def task_create_machine_specific_symlinks():
+    hostname = socket.gethostname()
+    for dictionary, target in [
+        (
+            {"dell": "wtfutil/config_home.yml", "opml-28": "wtfutil/config_work.yml"},
+            home / ".config/wtf/config.yml",
+        ),
+        (
+            {"dell": "git/.gitconfig", "opml-28": "git/.gitconfig_ifpilm"},
+            home / ".gitconfig",
+        ),
+        (
+            {"dell": "bash/.bashrc_local_home", "opml-28": "bash/.bashrc_local_ifpilm"},
+            home / ".bashrc_local",
+        ),
+    ]:
+        source = dictionary[hostname]
+        yield {
+            "name": source,
+            "file_dep": [pathlib.Path.cwd() / source],
+            "targets": [target],
+            "actions": [create_symlink2],
+        }
+
+        # link pre-commit/.pre-commit-config.yaml manually
