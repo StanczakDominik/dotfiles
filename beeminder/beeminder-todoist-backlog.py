50455ba5cf2ead962e81277ebb4e906f4f8b3187
Dominik Stańczak <stanczakdominik@gmail.com>
2020-05-28 09:32:46 +0200

    beeminder-todoist-backlog.py etc

---
+++ beeminder/beeminder-todoist-backlog.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+from beeminder import create_goal
+slugs = [
+    "todoist-backlog",
+    "todoist-unprioritized",
+    "todoist-breakdown",
+    "papers-backlog",
+    "youtube-backlog-upgrade",
+]
+goals = [create_goal(slug=slug) for slug in slugs]
+for goal in goals:
+    goal.update()
