21bfdf8c961be54ce10a64a4b856b0acad0b17e5
Dominik Stańczak <stanczakdominik@gmail.com>
2022-03-12 11:00:10 +0100

    Work script rework

--- scripts/workgoal.py
+++ scripts/workgoal.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 """Prints current work status in a single line."""
-from work import WorkTimer
+from work import WorkTimer, workhours
 import datetime
 import holidays
 import pathlib
+import json
+import os
 
 def workday_override(path="/tmp/WORKDAY"):
     path = pathlib.Path(path)
@@ -14,16 +16,16 @@ def workday_override(path="/tmp/WORKDAY"):
 
 
 
-def single_line():
-    timer = WorkTimer()
+list_entries = list(workhours.items())
+def single_line(index):
+    next_index = (index+1) % len(list_entries)
+    k, d = list_entries[index]
+    timer = WorkTimer(k, **d)
+    if timer.is_inactive_today:
+        return single_line(next_index)
     now = datetime.datetime.now().strftime("%X")
-    timer.describe_briefly(f"{now}: ")
+    print(timer.describe_briefly(f"{now}: "))
 
 if __name__ == "__main__":
-    holidays_pl = holidays.Polish()
-    today = datetime.date.today()
-    if (not workday_override()) and ((today in holidays_pl) or (today.weekday() >= 5)):
-        print("Enjoy your free day!")
-    else:
-        single_line()
+    single_line(os.environ.get("_state", 0))
 
