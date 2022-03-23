ea92e6fede660525f65f6b8552403dd5d609d9a5
Dominik Stańczak <stanczakdominik@gmail.com>
2022-03-23 18:59:23 +0100

    Adjust default imports for exploratory Jupyter work

--- ipython/profile_default/startup/10-filter-theano-numpy-warning.py
+++
@@ -1,8 +0,0 @@
-# as per https://github.com/Theano/Theano/issues/6667
-
-import warnings
-for message in [
-        "Argument backend_kwargs has not effect in matplotlib.plot_dist",
-        "Using a non-tuple sequence for multidimensional indexing is deprecated",
-]:
-    warnings.filterwarnings("ignore", message=message)
