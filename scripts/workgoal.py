66f8755f192454b45d566ac445898f0b08f0b7d7
Dominik Stańczak <stanczakdominik@gmail.com>
2022-06-14 06:41:13 +0200

    lsp-black

--- scripts/workgoal.py
+++ scripts/workgoal.py
@@ -7,20 +7,9 @@ import pathlib
 import json
 import os
 
-def workday_override(path="/tmp/WORKDAY"):
-    path = pathlib.Path(path)
-    try:
-        return float(path.read_text())
-    except:
-        return path.exists()
-
-
-
 list_entries = list(workhours.items())
 def single_line(index):
-    if index > (len(list_entries) + 1):
-        print("All done")
-        return
+    index = index % len(list_entries)
     next_index = (index+1)
     k, d = list_entries[index % len(list_entries)]
     timer = WorkTimer(k, **d)
@@ -28,9 +17,7 @@ def single_line(index):
         return single_line(next_index)
     now = datetime.datetime.now().strftime("%X")
     text = timer.describe_briefly(f"{now}: ")
-    # print(text)
     print(json.dumps({"_state": next_index, "full_text": text}))
-    # TODO json format, set color as percentage from red to white, or RGB for each goal
 
 if __name__ == "__main__":
     single_line(int(os.environ.get("_state", 0)))
