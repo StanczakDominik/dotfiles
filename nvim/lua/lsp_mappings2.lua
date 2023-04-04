4117089b875e080cfc36e02016dc35e67de37d68
Dominik Stańczak-Marikin <stanczakdominik@gmail.com>
2023-04-04 19:40:56 +0200

    Cleanup

--- nvim/lua/lsp_mappings2.lua
+++ nvim/lua/lsp_mappings2.lua
@@ -24,7 +24,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
 
     -- Lists all the implementations for the symbol under the cursor
-    bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
+    -- bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
 
     -- Jumps to the definition of the type symbol
     bufmap('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
@@ -40,6 +40,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
     -- Selects a code action available at the current cursor position
     bufmap('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
-    bufmap('x', '<F4>', '<cmd>lua vim.lsp.buf.range_code_action()<cr>')
+    bufmap('x', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
   end
 })
--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,7 +1,14 @@
 return {
 	{'vimwiki/vimwiki',
 	   init = function () --replace 'config' with 'init'
-		  vim.g.vimwiki_list = {{path = '~/Notes', syntax = 'markdown', ext = '.md', links_space_char='_', diary_rel_path='dziennik/daily'}}
+			vim.g.vimwiki_list = {
+				{ path = '~/Notes',
+				syntax = 'markdown',
+				ext = '.md',
+				links_space_char = '_',
+				diary_rel_path = 'dziennik/daily',
+			}
+			}
 	   end
 	},
 	{"ElPiloto/telescope-vimwiki.nvim",
