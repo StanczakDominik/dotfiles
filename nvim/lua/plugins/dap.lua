bc3def35a5db83f9f9ad5e773094a6785b70be59
Dominik Stańczak-Marikin <stanczakdominik@gmail.com>
2023-04-04 20:07:49 +0200

    dap.nvim

---
+++ nvim/lua/plugins/dap.lua
@@ -0,0 +1,44 @@
+return {
+	{"mfussenegger/nvim-dap",
+	config = function()
+			vim.fn.sign_define('DapBreakpoint',{ text ='🟥', texthl ='', linehl ='', numhl =''})
+			vim.fn.sign_define('DapStopped',{ text ='▶️', texthl ='', linehl ='', numhl =''})
+
+			vim.keymap.set('n', '<F5>', require 'dap'.continue)
+			vim.keymap.set('n', '<F10>', require 'dap'.step_over)
+			vim.keymap.set('n', '<F11>', require 'dap'.step_into)
+			vim.keymap.set('n', '<F12>', require 'dap'.step_out)
+			vim.keymap.set('n', '<leader>b', require 'dap'.toggle_breakpoint)
+		end
+	},
+	{"mfussenegger/nvim-dap-python", dependencies = {"mfussenegger/nvim-dap"},
+	config = function()
+		require("dap-python").setup("python")
+	end
+	},
+	{ "rcarriga/nvim-dap-ui",
+		dependencies = {"mfussenegger/nvim-dap"},
+		config = function()
+			local dap, dapui =require("dap"),require("dapui")
+			dapui.setup({})
+			dap.listeners.after.event_initialized["dapui_config"]=function()
+			  dapui.open()
+			end
+			dap.listeners.before.event_terminated["dapui_config"]=function()
+			  dapui.close()
+			end
+			dap.listeners.before.event_exited["dapui_config"]=function()
+			  dapui.close()
+			end
+		end
+		},
+		{"theHamsta/nvim-dap-virtual-text",
+		dependencies = {"mfussenegger/nvim-dap", "nvim-treesitter/nvim-treesitter"},
+		config = function()
+			require("nvim-dap-virtual-text").setup({
+				highlight_new_as_changed = true,
+				}
+			)
+		end
+	}
+}
