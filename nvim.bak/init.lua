df837a9728da284a98d4a980ba6e094e9d8eba06
Dominik Stańczak-Marikin <stanczakdominik@gmail.com>
2023-08-25 18:00:23 +0200

    add lazyvim starter

---
+++ nvim.bak/init.lua
@@ -0,0 +1,221 @@
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.wrap = false
+vim.opt.breakindent = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
+
+-- autocmd FileType make setlocal noexpandtab
+-- runtime ftplugin/man.vimi
+vim.opt.backspace='indent,eol,start'
+vim.opt.backupskip:append("*.asc")
+vim.opt.completeopt={'menu','menuone','noselect'}
+vim.opt.foldmethod='expr'
+vim.opt.foldexpr='nvim_treesitter#foldexpr()'
+vim.opt.foldenable=false
+vim.opt.hidden=true
+vim.opt.inccommand='split'
+vim.opt.ruler = true
+vim.opt.sessionoptions:append("tabpages,globals")
+vim.opt.showmatch = true
+vim.opt.showmode = true
+vim.opt.spelllang=pl,en_us
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.tabstop=4
+vim.opt.title = true
+-- vim.opt.viminfo+=n$HOME/.config/nvim/files/info
+-- vim.opt.viminfo='100,n$HOME/.config/nvim/files/info'
+vim.opt.wildmenu = true
+-- vim.g.mapleader = '\\'
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
+
+vim.opt.termguicolors = true
+
+require("mason").setup()
+
+-- vim.cmd('colorscheme solarized')
+
+
+require('lsp_setup')
+-- vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+--
+-- require('luasnip.loaders.from_vscode').lazy_load()
+--
+-- local cmp = require('cmp')
+-- local luasnip = require('luasnip')
+--
+-- local select_opts = {behavior = cmp.SelectBehavior.Select}
+--
+-- cmp.setup({
+--   snippet = {
+--     expand = function(args)
+--       luasnip.lsp_expand(args.body)
+--     end
+--   },
+--   sources = {
+--     {name = 'path'},
+--     {name = 'nvim_lsp', keyword_length = 3},
+--     {name = 'buffer', keyword_length = 3},
+--     {name = 'luasnip', keyword_length = 2},
+--   },
+--   window = {
+--     documentation = cmp.config.window.bordered()
+--   },
+--   formatting = {
+--     fields = {'menu', 'abbr', 'kind'},
+--     format = function(entry, item)
+--       local menu_icon = {
+--         nvim_lsp = 'λ',
+--         luasnip = '⋗',
+--         buffer = 'Ω',
+--         path = '🖫',
+--       }
+--
+--       item.menu = menu_icon[entry.source.name]
+--       return item
+--     end,
+--   },
+--   mapping = {
+--     ['<Up>'] = cmp.mapping.select_prev_item(select_opts),
+--     ['<Down>'] = cmp.mapping.select_next_item(select_opts),
+--
+--     ['<C-p>'] = cmp.mapping.select_prev_item(select_opts),
+--     ['<C-n>'] = cmp.mapping.select_next_item(select_opts),
+--
+--     ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+--     ['<C-f>'] = cmp.mapping.scroll_docs(4),
+--
+--     ['<C-e>'] = cmp.mapping.abort(),
+--     ['<CR>'] = cmp.mapping.confirm({select = false}),
+--
+--     ['<C-d>'] = cmp.mapping(function(fallback)
+--       if luasnip.jumpable(1) then
+--         luasnip.jump(1)
+--       else
+--         fallback()
+--       end
+--     end, {'i', 's'}),
+--
+--     ['<C-b>'] = cmp.mapping(function(fallback)
+--       if luasnip.jumpable(-1) then
+--         luasnip.jump(-1)
+--       else
+--         fallback()
+--       end
+--     end, {'i', 's'}),
+--
+--     ['<Tab>'] = cmp.mapping(function(fallback)
+--       local col = vim.fn.col('.') - 1
+--
+--       if cmp.visible() then
+--         cmp.select_next_item(select_opts)
+--       elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+--         fallback()
+--       else
+--         cmp.complete()
+--       end
+--     end, {'i', 's'}),
+--
+--     ['<S-Tab>'] = cmp.mapping(function(fallback)
+--       if cmp.visible() then
+--         cmp.select_prev_item(select_opts)
+--       else
+--         fallback()
+--       end
+--     end, {'i', 's'}),
+--   },
+-- })
+--
+--
+-- ---
+-- local builtin = require('telescope.builtin')
+-- vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+-- vim.keymap.set('n', '<C-P>', builtin.find_files, {})
+-- vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+-- vim.keymap.set('n', '<C-G>', builtin.live_grep, {})
+-- vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+-- vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+-- require('telescope').load_extension('fzf')
+--
+-- require('dap-python').setup('/usr/bin/python')
+-- require('dap-python').test_runner = 'pytest'
+--
+-- vim.cmd [[
+--     nnoremap <silent> <F5> <Cmd>lua require'dap'.continue()<CR>
+--     nnoremap <silent> <F10> <Cmd>lua require'dap'.step_over()<CR>
+--     nnoremap <silent> <F11> <Cmd>lua require'dap'.step_into()<CR>
+--     nnoremap <silent> <F12> <Cmd>lua require'dap'.step_out()<CR>
+--     nnoremap <silent> <Leader>b <Cmd>lua require'dap'.toggle_breakpoint()<CR>
+--     nnoremap <silent> <Leader>B <Cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>
+--     nnoremap <silent> <Leader>lp <Cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>
+--     nnoremap <silent> <Leader>dr <Cmd>lua require'dap'.repl.open()<CR>
+--     nnoremap <silent> <Leader>dl <Cmd>lua require'dap'.run_last()<CR>
+-- 	nnoremap <silent> <leader>dn :lua require('dap-python').test_method()<CR>
+-- 	nnoremap <silent> <leader>df :lua require('dap-python').test_class()<CR>
+-- 	vnoremap <silent> <leader>ds <ESC>:lua require('dap-python').debug_selection()<CR>
+-- ]]
+--
+-- require('nvim-treesitter.configs').setup({
+-- 	highlight = {
+-- 		enable = true,
+-- 		additional_vim_regex_highlighting = { "markdown" },
+-- 	},
+--   textobjects = {
+--     select = {
+--       enable = true,
+--       lookahead = true,
+--       keymaps = {
+--         ['af'] = '@function.outer',
+--         ['if'] = '@function.inner',
+--         ['ac'] = '@class.outer',
+--         ['ic'] = '@class.inner',
+--       }
+--     },
+--   },
+--   ensure_installed = {
+--     'python',
+--     'css',
+--     'json',
+--     'lua',
+-- 	"markdown",
+-- 	"markdown_inline",
+--   },
+-- })
+--
+-- require('toggleterm').setup({
+--   open_mapping = '<leader><C-g>',
+--   direction = 'horizontal',
+--   shade_terminals = true
+-- })
+--
+-- vim.keymap.set(
+--   "n",
+--   "gf",
+--   function()
+--     if require('obsidian').util.cursor_on_markdown_link() then
+--       return "<cmd>ObsidianFollowLink<CR>"
+--     else
+--       return "gf"
+--     end
+--   end,
+--   { noremap = false, expr = true}
+-- )
